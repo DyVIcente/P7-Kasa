944e0be585dfd784bd73afcbf59788a9e25c86f5
Vicente  Dyl <95346160+DyVIcente@users.noreply.github.com>
2022-11-08 16:36:22 +0100

    cards

--- src/components/Cards/card.jsx
+++ src/components/Cards/card.jsx
@@ -6,13 +6,13 @@ import Data from '../../data/data.json'
 export default class Card extends Component {
   render() {
     return (
-      <div>
+      <div className="losCardos">
         {Data &&
           Data.map((data) => {
             return (
               <Link className="cards" to={data.id} key={data.id}>
-                <img src={data.cover} alt=""></img>
-                <p>{data.title}</p>
+                <img className="imgCards" src={data.cover} alt=""></img>
+                <p className="titleCards">{data.title}</p>
               </Link>
             )
           })}
