753fc5e8240d044d4c963867b148f6e44bbdc530
Vicente  Dyl <95346160+DyVIcente@users.noreply.github.com>
2022-11-17 16:53:13 +0100

    carrousel

--- src/components/Caroussel/caroussel.jsx
+++ src/components/Caroussel/caroussel.jsx
@@ -1,19 +1,43 @@
 import './caroussel.css'
-import Data from '../../data/data.json'
+import { useState } from 'react'
+import VectorLeft from '../../assets/Vectorleft.png'
+import VectorRight from '../../assets/Vectorright.png'
 
-function Caroussel() {
+function Caroussel({ pictures }) {
+  const [current, setCurrent] = useState(0)
+  const length = pictures.length
+
+  const goToPrevious = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1)
+  }
+
+  const goToNext = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1)
+  }
+
+  if (!Array.isArray(pictures) || pictures.length <= 0) {
+    return null
+  }
+
   return (
-    <div>
-      {Data &&
-        Data.map((data) => {
+    <div className="cont">
+      <div>
+        <img className="vecR" src={VectorRight} alt="" onClick={goToNext} />
+        <img className="vecL" src={VectorLeft} alt="" onClick={goToPrevious} />
+      </div>
+      <div className="carou">
+        {pictures.map((picture, index) => {
           return (
-            <div key={data.id}>
-              <img src={data.pictures[0]} alt="slide"></img>
+            <div
+              className={index === current ? 'picture active' : 'picture'}
+              key={index}
+            >
+              <img className="carouImg" key={index} src={picture} alt=""></img>
             </div>
           )
         })}
+      </div>
     </div>
   )
 }
-
 export default Caroussel
--- src/pages/Logement/logement.jsx
+++ src/pages/Logement/logement.jsx
@@ -6,6 +6,7 @@ import { useParams } from 'react-router-dom'
 import Logementtitle from '../../components/LogementTitle/logementtitle.jsx'
 import Tags from '../../components/Tags/tags'
 import Host from '../../components/Host/host.jsx'
+import Caroussel from '../../components/Caroussel/caroussel'
 
 function Logement() {
   // Hook useParams pour recup l'id dans l'url
@@ -13,16 +14,24 @@ function Logement() {
   // On utilise find pour trouver le premier produit qui repond a la demande
   const logemenData = Data.find((product) => product.id === id)
 
-  const { title, location, tags, host, rating, description, equipments } =
-    logemenData
+  const {
+    pictures,
+    title,
+    location,
+    tags,
+    rating,
+    host,
+    description,
+    equipments,
+  } = logemenData
 
   return (
     <div>
-      <p>image slider</p>
+      <Caroussel pictures={pictures} />
       <Logementtitle title={title} location={location} />
       <Tags tags={tags} />
       <div className="starHost">
-        <Stars />
+        <Stars stars={rating} />
         <Host host={host} />
       </div>
       <div className="dropStyle">
