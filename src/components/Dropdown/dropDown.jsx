2568d4fa29b2462d10e514d7abbf48c3501e431f
Vicente  Dyl <95346160+DyVIcente@users.noreply.github.com>
2022-11-23 16:58:23 +0100

    css & vector

--- src/components/Dropdown/dropDown.jsx
+++ src/components/Dropdown/dropDown.jsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import './dropDown.css'
 import Vector from '../../assets/VectorDown.png'
+import VectorUp from '../../assets/Vectorup.png'
 import { useState } from 'react'
 
 function Dropdown({ title, description }) {
@@ -16,7 +17,11 @@ function Dropdown({ title, description }) {
     <div className="btn-menu">
       <button className="btn" onClick={handleOpen}>
         <p className="btn-txt">{title}</p>
-        <img className="vectorDown" src={Vector} alt=""></img>
+        {open ? (
+          <img className="vectorUp" src={VectorUp} alt=""></img>
+        ) : (
+          <img className="vectorDown" src={Vector} alt=""></img>
+        )}
       </button>
       {open ? (
         <ul className="menu">
