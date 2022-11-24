6606b229bfcb3098df3afbd9c020bfd0308c3795
Vicente  Dyl <95346160+DyVIcente@users.noreply.github.com>
2022-11-24 16:51:23 +0100

    somecss

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -6,14 +6,14 @@ function Header() {
   return (
     <nav className="navBar">
       <img className="logoNav" src={kasa} alt="logo kasa" />
-      <div className="navMenu">
+      <nav className="navMenu">
         <Link className="navTitle" to="/">
           Accueil
         </Link>
         <Link className="navTitle" to="/about">
           A propos
         </Link>
-      </div>
+      </nav>
     </nav>
   )
 }
